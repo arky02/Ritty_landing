0f0f10504195da33bf80bac51dbd98be47b97f40
arky02 <kyean07@naver.com>
2025-03-09 18:20:39 +0900

    chore: initial setting from next-template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
 import "@/styles/globals.css";
+import "@/styles/fonts/pretendard.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary
+        state={pageProps.dehydratedState ?? { queries: [], mutations: [] }}
+      >
+        <Toaster
+          containerStyle={{ fontSize: "14px", fontWeight: "600" }}
+          toastOptions={{
+            style: {
+              padding: "8px 10px",
+              color: "#242424",
+              borderRadius: "999px",
+            },
+          }}
+        />
+        <Component {...pageProps} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
 }
