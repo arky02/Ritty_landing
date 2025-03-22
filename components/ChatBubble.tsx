11606649fac59b5d8f448af685a6bb010cae67f6
arky02 <kyean07@naver.com>
2025-03-23 02:15:23 +0900

    feat: 채팅 연속 입력 시 말풍선 연속으로 나타나게 구현

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -1,15 +1,32 @@
 import RittyChatProfile from "@/public/icons/ritty-chat-profile.svg";
 import Image from "next/image";
 
-function ChatBubble({ sender, msg }: { sender: string; msg: string }) {
-  return sender === "user" ? (
-    // User Message
+const UserChatBubble = ({ msg }: { msg: string }) => {
+  return (
     <div className="inline-block w-fit text-sm relative mx-0 md:my-[15px] my-[8px] bg-main float-right clear-both text-white py-[8px] px-[13px] rounded-[.875rem_.875rem_0_.875rem]">
       {msg}
     </div>
+  );
+};
+
+const RittyChatBubble = ({ msg }: { msg: string }) => {
+  return (
+    <div className="inline-block w-fit text-sm relative bg-white float-left clear-both text-[#000000] py-[8px] px-[13px] rounded-[.875rem_.875rem_.875rem_0] border-[1.4px] border-[#E7E9EF]">
+      {msg}
+    </div>
+  );
+};
+function ChatBubble({ sender, msg }: { sender: string; msg: string[] }) {
+  return sender === "user" ? (
+    // User Message
+    <div>
+      {msg.map((userMsg) => (
+        <UserChatBubble msg={userMsg} />
+      ))}
+    </div>
   ) : (
     // Ritty Message
-    <div className="flex w-fit items-center w-full">
+    <div className="flex w-fit items-end w-full">
       <Image
         src={RittyChatProfile}
         width={35}
@@ -17,17 +34,10 @@ function ChatBubble({ sender, msg }: { sender: string; msg: string }) {
         className="bg-contain h-[35px] mr-[10px]"
         alt="ritty-chat-profile"
       />
-      <div className="inline-block w-fit text-sm relative mx-0 my-[10px] bg-white float-left clear-both text-[#000000] py-[8px] px-[13px] rounded-[.875rem_.875rem_.875rem_0] border-[1.4px] border-[#E7E9EF]">
-        {msg === "ritty_loading_msg" ? (
-          // Loading Message
-          <div className="flex space-x-1 justify-center items-center  p-[7.2px] ">
-            <div className="h-[6px] w-[6px] bg-[#919191] rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-            <div className="h-[6px] w-[6px] bg-[#919191] rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-            <div className="h-[6px] w-[6px] bg-[#919191] rounded-full animate-bounce"></div>
-          </div>
-        ) : (
-          msg
-        )}
+      <div className="flex flex-col gap-[2px]">
+        {msg.map((rittyMsg) => (
+          <RittyChatBubble msg={rittyMsg} />
+        ))}
       </div>
     </div>
   );
