fd62a822d3cf000c73aa89cd164340e098ee6a79
yeonkimm <ky.kim@nation-a.com>
2025-04-06 00:34:30 +0900

    feat: 리티 살짝 작게 수정

--- components/layout/ThreeJSRitty.tsx
+++ components/layout/ThreeJSRitty.tsx
@@ -8,10 +8,9 @@ import { useState } from 'react';
 
 const ThreeJSRitty = () => {
 
-  
-  
+
   const cameraStates = {
-    position: [0, 65, 20] as Vector3Tuple, // 정확히 위에서 내려다보기
+    position: [0, 70, 20] as Vector3Tuple, // 정확히 위에서 내려다보기
     target: [0, 10, 0] as Vector3Tuple,  // 카메라 시선
   }
 
